10b2017cf50cf65e73c50552397172ef069ca5c4
Oscar Méndez <48070861+Oscarmendez825@users.noreply.github.com>
2024-04-08 16:03:35 -0600

    [feat]: Add installation script for dependencies

---
+++ ImageProcessing/installer.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo Instalando OpenCV...
+pip install opencv-python
+
+echo Instalando NumPy...
+pip install numpy
+
+echo Instalando scikit-image...
+pip install scikit-image
+
+echo Instalación completa.
