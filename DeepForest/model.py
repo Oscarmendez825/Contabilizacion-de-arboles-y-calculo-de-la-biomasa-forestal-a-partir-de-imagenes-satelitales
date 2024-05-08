c99e62139ea95a7ef92c15dc088b182a04a765c1
Oscar Méndez <48070861+Oscarmendez825@users.noreply.github.com>
2024-05-07 19:59:26 -0600

    [feat] Complete grid search for hyperparameter optimization
    This commit marks the completion of the grid search process for hyperparameter optimization. The grid search has been conducted to find the best combination of hyperparameters for the model.

--- DeepForest/model.py
+++ DeepForest/model.py
@@ -2,10 +2,11 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 from ImageProcessing import Filters, Preprocessing
 from deepforest import main as mn
 import pandas as pd
+import cv2
 import os
 
 
-def pre_process_image(image_path, tif_path):
+def pre_process_image(image_path, tif_path, image_filter=None):
     """
     Converts an RGBA image to RGB and then to TIFF format.
 
@@ -17,8 +18,14 @@ def pre_process_image(image_path, tif_path):
     None
     """
     png_path = "./tempImage.png"
-    Preprocessing.convert_rgba_to_rgb(image_path, png_path)
-    Preprocessing.convert_png_to_tiff(png_path, tif_path)
+    if image_filter is not None:
+        image = Filters.select_and_execute(image_path, image_filter)
+        cv2.imwrite(png_path, image)
+        Preprocessing.convert_rgba_to_rgb(png_path, png_path)
+        Preprocessing.convert_png_to_tiff(png_path, tif_path)
+    else:
+        Preprocessing.convert_rgba_to_rgb(image_path, png_path)
+        Preprocessing.convert_png_to_tiff(png_path, tif_path)
 
 
 def count_tree(predictions):
@@ -35,7 +42,7 @@ def count_tree(predictions):
     return tree_number
 
 
-def predict_image(path, model):
+def predict_image(path, model, patch_size=400):
     """
     Predicts the number of trees in an image using the DeepForest model and prints the result.
 
@@ -43,19 +50,19 @@ def predict_image(path, model):
     path (str): The path to the image.
 
     Returns:
-    None
+    tree_value (int): number of trees
     """
-    predictions = model.predict_tile(raster_path=path, return_plot=False, patch_size=550)
+    predictions = model.predict_tile(raster_path=path, return_plot=False, patch_size=patch_size)
     tree_value = count_tree(predictions)
     return tree_value
 
 
-def evaluate_predictions(test_data, model):
+def evaluate_predictions(test_data, model, image_filter=None, patch_size=400):
     """
     Evaluates the predictions made by the model on test data.
 
     Parameters:
-    test_data (DataFrame): DataFrame containing test data with columns 'Title' (image path)
+    test_data (DataFrame): DataFrame containing test data with columns 'Title' (image name)
                            and 'TreeCrowns' (actual number of tree crowns).
 
     Returns:
@@ -69,8 +76,8 @@ def evaluate_predictions(test_data, model):
         image_path = "../Data/Images/"+row['Title']+".png"
         actual_value = row['TreeCrowns']
         gen_tif_path = "./tempImage.tif"
-        pre_process_image(image_path, gen_tif_path)
-        predicted_value = predict_image(gen_tif_path, model)
+        pre_process_image(image_path, gen_tif_path, image_filter)
+        predicted_value = predict_image(gen_tif_path, model, patch_size)
         actual_values.append(actual_value)
         predicted_values.append(predicted_value)
         os.remove(gen_tif_path)
@@ -86,14 +93,42 @@ def evaluate_predictions(test_data, model):
     return mae, mse
 
 
-def test_deep_forest(model):
+def test_deep_forest(model, image_filter=None, patch_size=400):
     # Read test data from CSV file
     test_data = pd.read_csv("../Data/TestData.csv", sep=";")
     # Call the evaluate_predictions function to get MAE and MSE
-    mae, mse = evaluate_predictions(test_data, model)
-
+    mae, mse = evaluate_predictions(test_data, model, image_filter, patch_size)
     print("Mean Absolute Error:", mae)
     print("Mean Squared Error:", mse)
+    return mae, mse
+
+
+def grid_search(model, patch_size):
+    image_filters = [None, "histogram_equalization", "median_filter", "gaussian_filter", "bilateral_filter", "background_removal"]
+
+    best_mae = float('inf')
+    best_mse = float('inf')
+    best_image_filter = None
+
+    for image_filter in image_filters:
+        print("patch_size:", patch_size)
+        print("filter:", image_filter)
+        mae, mse = test_deep_forest(model, image_filter=image_filter, patch_size=patch_size)
+
+        if mae < best_mae:
+            best_mae = mae
+            best_mse = mse
+            best_image_filter = image_filter
+
+    print("Best Mean Absolute Error:", best_mae)
+    print("Best Mean Squared Error:", best_mse)
+    print("Best Image Filter:", best_image_filter)
+
+    with open("results.txt", "a") as file:
+        file.write("Patch Size: {}\n".format(patch_size))
+        file.write("Best Mean Absolute Error: {}\n".format(best_mae))
+        file.write("Best Mean Squared Error: {}\n".format(best_mse))
+        file.write("Best Image Filter: {}\n\n".format(best_image_filter))
 
 
 def main():
@@ -105,8 +140,8 @@ def main():
     """
     model = mn.deepforest()
     model.use_release(check_release=False)
-
-    test_deep_forest(model)
+    grid_search(model, patch_size=515)
+    # test_deep_forest(model, image_filter="histogram_equalization")
 
 
 if __name__ == '__main__':
